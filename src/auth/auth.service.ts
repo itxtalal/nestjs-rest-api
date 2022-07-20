2ed63d76a75e79c7d792ef07685f8dd55069b3e0
Talal <talalstudent@gmail.com>
2022-07-20 14:12:27 +0500

    nestjs pipes - validation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { User, Bookmark } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
 
 @Injectable({})
 export class AuthService {
   constructor(private prismaService: PrismaService) {}
-  signup() {
+  signup(dto: AuthDto) {
+    console.log({ dto });
     return { message: 'Signup  from AuthService' };
   }
   signin() {
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    // Whitelist true only accepts the fields that are defined in the schema, and the extra fields are ignored.
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
   await app.listen(3333);
 }
 bootstrap();
