b806b4a21094f01874252e9d3640dabc3fad75bc
Talal <talalstudent@gmail.com>
2022-07-20 16:23:11 +0500

    signup logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,39 @@
-import { Injectable } from '@nestjs/common';
-import { User, Bookmark } from '@prisma/client';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable({})
 export class AuthService {
   constructor(private prismaService: PrismaService) {}
-  signup(dto: AuthDto) {
-    console.log({ dto });
-    return { message: 'Signup  from AuthService' };
+  async signup(dto: AuthDto) {
+    // console.log({ dto });
+
+    // hash the password
+    const hash = await argon.hash(dto.password);
+
+    try {
+      // create a new user in the database
+      const user = await this.prismaService.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+
+      delete user.hash;
+
+      // return the created user
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Email already in use');
+        }
+      }
+      throw error;
+    }
   }
   signin() {
     return { message: 'Signin from AuthService' };
